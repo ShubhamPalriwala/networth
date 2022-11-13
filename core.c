0a4f0935fa749c3e7db0633eca595dafbfec92f4
ShubhamPalriwala <spalriwalau@gmail.com>
2022-11-13 13:26:26 +0530

    feat: switch to bpf ring buffer for per-event-processing

--- core.c
+++ core.c
@@ -2,7 +2,13 @@
 #include <linux/bpf.h>
 #include <linux/ip.h>
 
-BPF_HISTOGRAM(counter, unsigned int);
+struct ip_and_byte_ds
+{
+    unsigned int ip;
+    unsigned int bytes;
+};
+
+BPF_RINGBUF_OUTPUT(events, 128);
 
 int count_network_bytes_per_ip(struct xdp_md *ctx)
 {
@@ -20,11 +26,19 @@ int count_network_bytes_per_ip(struct xdp_md *ctx)
         // Check if the IP packet is within the bounds of ethernet frame
         if ((void *)ip_packet + sizeof(*ip_packet) <= ethernet_end)
         {
+            struct ip_and_byte_ds *ip_and_byte = events.ringbuf_reserve(sizeof(struct ip_and_byte_ds));
+            if (!ip_and_byte)
+            {
+                return 1;
+            }
 
-            unsigned int value = ip_packet->saddr;
+            unsigned int source_ip = ip_packet->saddr;
             unsigned int ethernet_size_in_bytes = (ethernet_end - ethernet_start);
 
-            counter.increment(value, ethernet_size_in_bytes);
+            ip_and_byte->ip = source_ip;
+            ip_and_byte->bytes = ethernet_size_in_bytes;
+
+            events.ringbuf_submit(ip_and_byte, 0);
         }
     }
 
